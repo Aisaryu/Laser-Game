caf32aec83920a5e00f8c14b17d92edb2bc6fd07
Noelle Bryan Antinero <136225350+Aisaryu@users.noreply.github.com>
2024-11-26 18:37:00 +0800

    Add files via upload

---
+++ oopoop/src/laserGame/GameFrame.java
@@ -0,0 +1,28 @@
+package laserGame;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GameFrame extends JFrame implements ActionListener{
+
+    /**
+     * Default Constructor
+     */
+    @SuppressWarnings("static-access")
+    public GameFrame() {
+        super("The Floppy Game");
+        this.setSize(800,750);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Listens for button presses
+     * @param e
+     *  takes in action event
+     */
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
